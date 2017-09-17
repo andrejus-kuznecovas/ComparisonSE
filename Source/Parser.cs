66f2c52a8c26bc0b81aad2f27eb28ff8d4e9221c
Tomas Mūžas <m_tomas@inbox.lt>
2017-09-17 19:23:57 +0300

    Add comments

--- Source/Parser.cs
+++ Source/Parser.cs
@@ -6,19 +6,19 @@ namespace CSE
     public class Parser
     {
         public static Shop GetShopName(string text) {
-            foreach (string shopName in Enum.GetNames(typeof(Shop)))
+            foreach (string shopName in Enum.GetNames(typeof(Shop)))  // loop through all the shops (their names)
             {
                 if (shopName != "UNKNOWN_SHOP") {
-                    string shopPattern = "\\b" + shopName.ToLower() + "\\b";
+                    string shopPattern = "\\b" + shopName.ToLower() + "\\b"; // look for the name of enum as one word
 
                     bool matching = Regex.IsMatch(text, shopPattern, RegexOptions.IgnoreCase);
                     if (matching)
                     {
-                        return (Shop)Enum.Parse(typeof(Shop), shopName);
+                        return (Shop)Enum.Parse(typeof(Shop), shopName); // return enum parsed from the shop name
                     }
                 }
             }
-            return Shop.UNKNOWN_SHOP;
+            return Shop.UNKNOWN_SHOP; // if nothing is found, return unknown shop
         }
     }
 }
