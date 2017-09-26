861cb3eccd3ec67f751c6806515aff19775e7420
UrbanAndrius <UrbanAndrius@users.noreply.github.com>
2017-09-26 22:00:48 +0300

    Implement Item class
    Item class with basic characteristics

---
+++ Item.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSE
+{
+    class Item
+    {
+        private static uint itemCount = 0;
+
+        private double price;
+        private string name;
+        private string typeName;
+        private uint typeId;
+
+
+        public Item(double price, string name, string typeName)
+        {
+            this.price = price;
+            this.name = name;
+            this.typeName = typeName;
+        }
+
+        public Item(double price, string name, uint typeId)
+        {
+            this.price = price;
+            this.name = name;
+            this.typeId = typeId;
+        }
+
+        public double getPrice()
+        {
+            return this.price;
+        }
+
+        public string getName()
+        {
+            return this.name;
+        }
+
+        public string getTypeName()
+        {
+            return this.typeName;
+        }
+
+        public double getTypeID()
+        {
+            return this.typeId;
+        }
+
+        ~Item()
+        {
+
+        }
+    }
+}
