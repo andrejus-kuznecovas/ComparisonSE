24caee7c15a51b89fe456feb4ad5479f588807d5
Tomas Mūžas <m_tomas@inbox.lt>
2017-09-26 21:57:26 +0300

    Add basic statistics functionality

---
+++ Source/DataSet.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSE.Source
+{
+    public class DataSet : Dictionary<float, DateTime>
+    {
+        public void Filter(Period period)
+        {
+            IEnumerable<KeyValuePair<float, DateTime>> filteredData;
+            switch (period)
+            {
+                case Period.DAY:
+                    filteredData = this.Where(
+                        item =>
+                            item.Value == DateTime.Today
+                    );
+                    break;
+
+                case Period.WEEK:
+                    int delta = DayOfWeek.Monday - DateTime.Today.DayOfWeek;
+                    DateTime monday = DateTime.Today.AddDays(delta);
+                    DateTime sunday = monday.AddDays(6);
+
+                    filteredData = this.Where(
+                        item =>
+                            item.Value >= monday &&
+                            item.Value <= sunday
+                    );
+                    break;
+
+                case Period.MONTH:
+                    filteredData = this.Where(
+                        item =>
+                            item.Value.Month == DateTime.Today.Month &&
+                            item.Value.Year == DateTime.Today.Year
+                    );
+                    break;
+
+
+                case Period.YEAR:
+                    filteredData = this.Where(
+                        item =>
+                            item.Value.Month == DateTime.Today.Month &&
+                            item.Value.Year == DateTime.Today.Year
+                    );
+                    break;
+
+                case Period.DEFAULT:
+                default:
+                    filteredData = this.AsEnumerable<KeyValuePair<float, DateTime>>();
+                    break;
+            }
+            this.Clear();
+            foreach (KeyValuePair<float, DateTime> item in filteredData)
+            {
+                this.Add(item.Key, item.Value);
+            }
+            
+        }
+    }
+}
---
+++ Source/Statistics.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSE.Source
+{
+    public class Statistics
+    {
+        public static DataSet GetData(Period period = Period.DEFAULT)
+        {
+            DataSet data = new DataSet();
+            data.Add(1.5f, new DateTime(2017, 9, 26));
+            data.Add(1.7f, new DateTime(2017, 9, 27));
+            data.Add(1.9f, new DateTime(2017, 9, 28));
+            data.Filter(period);
+            return data;
+        }
+
+    }
+
+
+    public enum Period {
+        DEFAULT, DAY, WEEK, MONTH, YEAR 
+    }
+
+}
+
