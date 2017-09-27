47b4129f31694dbc32282acf4387d9db29fcd432
Tomas Mūžas <m_tomas@inbox.lt>
2017-09-27 23:22:37 +0300

    Improve DataSet

--- Source/DataSet.cs
+++ Source/DataSet.cs
@@ -4,69 +4,76 @@ using System.Linq;
 
 namespace CSE.Source
 {
-    public class DataSet<TKey, TValue> : Dictionary<TKey, TValue>
+    public class DataSet
     {
-        //public void Filter(Period period)
-        //{
-        //    IEnumerable<KeyValuePair<TKey, TValue>> filteredData;
-        //    switch (period)
-        //    {
-        //    #region Filter By Day
-        //        case Period.DAY:
-        //            filteredData = this.Where(
-        //                item =>
-        //                    item.Value == DateTime.Today
-        //            );
-        //            break;
-        //    #endregion
-        //    #region Filter By Week
+        private List<Receipt> receipts;
 
-        //        case Period.WEEK:
-        //            int delta = DayOfWeek.Monday - DateTime.Today.DayOfWeek;
-        //            DateTime monday = DateTime.Today.AddDays(delta);
-        //            DateTime sunday = monday.AddDays(6);
+        public DataSet()
+        {
+            receipts = new List<Receipt>();
+        }
 
-        //            filteredData = this.Where(
-        //                item =>
-        //                    item.Value >= monday &&
-        //                    item.Value <= sunday
-        //            );
-        //            break;
-        //    #endregion
-        //    #region Filter By Month
-        //        case Period.MONTH:
-        //            filteredData = this.Where(
-        //                item =>
-        //                    item.Value.Month == DateTime.Today.Month &&
-        //                    item.Value.Year == DateTime.Today.Year
-        //            );
-        //            break;
-        //    #endregion
-        //    #region Filter By Year
-        //        case Period.YEAR:
-        //            filteredData = this.Where(
-        //                item =>
-        //                    item.Value.Month == DateTime.Today.Month &&
-        //                    item.Value.Year == DateTime.Today.Year
-        //            );
-        //            break;
-        //    #endregion
-        //    #region Default Filter
+        public void Filter(Period period)
+        {
+            IEnumerable<Receipt> filteredData;
+            switch (period)
+            {
+                #region Filter By Day
+                case Period.DAY:
+                    filteredData = this.receipts.Where(
+                        receipt =>
+                            receipt.purchaseTime == DateTime.Today
+                    );
+                    break;
+                #endregion
+                #region Filter By Week
 
-        //        case Period.DEFAULT:
-        //        default:
-        //            filteredData = this.AsEnumerable<KeyValuePair<float, DateTime>>();
-        //            break;
-        //    #endregion
-        //    }
+                case Period.WEEK:
+                    int delta = DayOfWeek.Monday - DateTime.Today.DayOfWeek;
+                    DateTime monday = DateTime.Today.AddDays(delta);
+                    DateTime sunday = monday.AddDays(6);
+
+                    filteredData = this.receipts.Where(
+                        receipt =>
+                            receipt.purchaseTime >= monday &&
+                            receipt.purchaseTime <= sunday
+                    );
+                    break;
+                #endregion
+                #region Filter By Month
+                case Period.MONTH:
+                    filteredData = this.receipts.Where(
+                        receipt =>
+                            receipt.purchaseTime.Month == DateTime.Today.Month &&
+                            receipt.purchaseTime.Year == DateTime.Today.Year
+                    );
+                    break;
+                #endregion
+                #region Filter By Year
+                case Period.YEAR:
+                    filteredData = this.receipts.Where(
+                        receipt =>
+                            receipt.purchaseTime.Month == DateTime.Today.Month &&
+                            receipt.purchaseTime.Year == DateTime.Today.Year
+                    );
+                    break;
+                #endregion
+                #region Default Filter
+
+                case Period.DEFAULT:
+                default:
+                    filteredData = this.receipts;
+                    break;
+                    #endregion
+            }
 
 
-        //    this.Clear();
-        //    foreach (KeyValuePair<TKey, TValue> item in filteredData)
-        //    {
-        //        this.Add(item.Key, item.Value);
-        //    }
-            
-        //}
+            this.receipts.Clear();
+            foreach (Receipt receipt in filteredData)
+            {
+                this.receipts.Add(receipt);
+            }
+
+        }
     }
 }
--- Source/Receipt.cs
+++ Source/Receipt.cs
@@ -5,7 +5,7 @@ using System.Runtime.Serialization;
 
 namespace CSE.Source
 {
-    [Serializable]
+    [Serializable()]
     class Receipt: ISerializable
     {
         private string initialText;
