6533c0a8f1c27c6c52a2576171194c456fe36128
tomasmuzas <m_tomas@inbox.lt>
2017-10-20 01:02:02 +0300

    Add Tesseract API support + Clean up the project

---
+++ Login/Source/UI/Dialog.cs
@@ -0,0 +1,106 @@
+using System;
+using Android.App;
+using Android.OS;
+using Android.Views;
+using Android.Widget;
+
+namespace Login
+{
+    public class OnSignUpEventArgs : EventArgs
+    {
+        private string name;
+        private string surname;
+        private string email;
+        private string username;
+        private string password;
+
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+
+        public OnSignUpEventArgs(string name, string surname, string email, string username,string password ):base()
+        {
+            this.name = name;
+            this.surname = surname;
+            this.email = email;
+            this.username = username;
+            this.password = password;
+
+        }
+        
+    }
+    public class Dialog : DialogFragment
+    {
+        private EditText name;
+        private EditText surname;
+        private EditText email;
+        private EditText username;
+        private EditText password;
+        private Button signUpBtn;
+
+        public EventHandler<OnSignUpEventArgs> signUpComplete;
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+             base.OnCreateView(inflater, container, savedInstanceState);
+            var view = inflater.Inflate(Resource.Layout.dialog_sign_up, container, false);
+
+            name = view.FindViewById<EditText>(Resource.Id.popup_name);
+            surname = view.FindViewById<EditText>(Resource.Id.popup_surname);
+            email = view.FindViewById<EditText>(Resource.Id.popup_email);
+            username = view.FindViewById<EditText>(Resource.Id.popup_username);
+            password = view.FindViewById<EditText>(Resource.Id.popup_password);
+            
+         
+
+            signUpBtn = view.FindViewById<Button>(Resource.Id.popup_register_button);
+
+            signUpBtn.Click += SignUpBtn_Click;
+
+            return view;
+        }
+
+        private void SignUpBtn_Click(object sender, EventArgs e)
+        {
+           
+                signUpComplete.Invoke(this, new OnSignUpEventArgs(name.Text, surname.Text, email.Text, username.Text, password.Text));
+                this.Dismiss();
+           
+        }
+
+        public override void OnActivityCreated(Bundle savedInstanceState)
+        {
+            Dialog.Window.RequestFeature(WindowFeatures.NoTitle);
+            base.OnActivityCreated(savedInstanceState);
+            Dialog.Window.Attributes.WindowAnimations = Resource.Style.dialog_anim;
+        }
+
+
+     
+    }
+   
+
+}
---
+++ Login/Source/UI/WelcomeScreen.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+
+using Android.Widget;
+using Login.Source.Controllers;
+
+namespace Login
+{
+    [Activity(Theme = "@style/Theme.Brand")]
+    class WelcomeScreen : Activity
+    {
+        private ImageView imageView;
+        private Button photoButton;
+        private TextView welcomeText;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+
+            SetContentView(Resource.Layout.first);
+            welcomeText = FindViewById<TextView>(Resource.Id.logged_in_welcome_text);
+            if (UserController.IsUserInitialized())
+            {
+                string name = UserController.UserName;
+                string surname = UserController.UserSurname;
+                welcomeText.Text = String.Format("{0}, {1} {2}", Resources.GetString(Resource.String.welcome_text), name, surname );
+            }
+            imageView = FindViewById<ImageView>(Resource.Id.imageView);
+            photoButton = FindViewById<Button>(Resource.Id.photoButton);
+            photoButton.Click += PhotoButton_Click;
+        }
+
+        private void PhotoButton_Click(object sender, EventArgs e)
+        {
+            Intent intent = new Intent(this, typeof(CameraStream));
+            StartActivity(intent);
+
+        }
+
+    }
+
+
+}
+//ToolBar reikes ateityje
+/*
+drawerLayout = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+leftDrawer = FindViewById<ListView>(Resource.Id.left_drawer);
+leftItems.Add("Vienas");
+leftItems.Add("Du");
+leftItems.Add("Trys");
+leftItems.Add("Keturi");
+leftItems.Add("Penki");
+leftItems.Add("Sesi");
+leftItems.Add("Septyni");
+
+
+leftAdapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, leftItems);
+drawerToggle = new MyActionBarDrawerToggle(this, drawerLayout, Resource.String.open_drawer, Resource.String.close_drawer);
+leftDrawer.Adapter = leftAdapter;
+
+drawerLayout.SetDrawerListener(drawerToggle);
+
+mToolbar = FindViewById<SupportToolbar>(Resource.Id.toolbar);
+SetSupportActionBar(mToolbar);
+
+SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+SupportActionBar.SetHomeButtonEnabled(true);
+SupportActionBar.SetDisplayShowTitleEnabled(true);
+}
+else
+{
+
+}
+*/
