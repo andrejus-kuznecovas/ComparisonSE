4750add4b62eabc45248c0002c350fe636b970a8
tomasmuzas <m_tomas@inbox.lt>
2017-10-29 00:35:08 +0300

    Set foundation for Scanbot OCR

---
+++ Login/Source/Controllers/OCR/ImageRecognitionScanbot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Login.Source.Controllers.OCR
+{
+    class ImageRecognitionScanbot
+    {
+    }
+}
