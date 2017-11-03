da5278dfe611bc638fc94ab81d8b21efb0ce9f75
tomasmuzas <m_tomas@inbox.lt>
2017-11-03 14:51:31 +0200

    Add legacy files

---
+++ Login/Source/Controllers/Parsing/Parser.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Login
+{
+    public class Parser
+    {
+        public static Shop GetShopName(string text) {
+            Dictionary<Shop, string[]> shopUniqueTexts = new Dictionary<Shop, string[]>();
+            shopUniqueTexts.Add(Shop.IKI, new string[]{ "palink", "iki"});
+            shopUniqueTexts.Add(Shop.MAXIMA, new string[] { @"maxim(a|[^u]\w+)"});
+            shopUniqueTexts.Add(Shop.RIMI, new string[] { "rimi" });
+            shopUniqueTexts.Add(Shop.LIDL, new string[] { @"lidl\w*" });
+            shopUniqueTexts.Add(Shop.NORFA, new string[] { @"norf\w+" });
+            string receiptText = Parser.RemoveInternationalLetters(text);
+
+            foreach (KeyValuePair<Shop,string[]> shopInfo in shopUniqueTexts)  // loop through all the shops (their names)
+            {
+                foreach(string shopIdentifier in shopInfo.Value) {
+                    string shopPattern = @"\b" + shopIdentifier.ToLower() + @"\b"; // look for the name of enum as one word
+                    Shop currentShop = shopInfo.Key;
+    
+                    bool matching = Regex.IsMatch(receiptText, shopPattern, RegexOptions.IgnoreCase);
+                    if (matching)
+                    {
+                        return currentShop; // return Shop
+                    }
+                }
+            }
+            return Shop.UNKNOWN_SHOP; // if nothing is found, return unknown shop
+        }
+
+        public static float ExtractPriceFloat(string text) {
+            string pricePattern = @"(-?\d+(\.|,)\s?\d{1,2})\s?(A|N)\b"; // match price-formatted float
+            float result;
+            Match priceMatch = Regex.Match(RemoveInternationalLetters(text),
+                pricePattern, RegexOptions.IgnoreCase);
+            try
+            {
+                string preparedMatch = priceMatch.Groups[1].Value.Replace(" ","");
+                preparedMatch = preparedMatch.Replace(',', '.'); // replace , to . for parsing
+                result = float.Parse(preparedMatch);
+                return result;
+            }
+            catch(FormatException e) {
+                return -1000f;
+            }
+        }
+
+        // Replace international letter with its latin form (e.g. Š->S)
+        public static string RemoveInternationalLetters(string text)
+        {
+            return String.Join("", text.Normalize(NormalizationForm.FormD)
+                .Where(c => char.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark));
+        }
+
+        public static string RemoveNonLetters(string text)
+        {
+            string patternForPrice = @"(-?\d+(\.|,)\s?\d{1,2})\s?(A|N)\b";
+            string textWithoutPrice = Regex.Replace(text, patternForPrice, "");
+            textWithoutPrice = RemoveInternationalLetters(textWithoutPrice);
+            string patternForLetters = @"[^a-zA-Z\s]";
+            string replaced = Regex.Replace(textWithoutPrice, patternForLetters, "");
+            return RemoveTrailingWhitespace(replaced);
+        }
+
+        public static string RemoveTrailingWhitespace(string text)
+        {
+            string whitespacePattern = @"\s+$";
+            return Regex.Replace(text, whitespacePattern, "");
+        }
+
+        public static bool FindEurKg(string text)
+        {
+            if (text.Contains("EUR/kg"))
+                return true;
+            else return false;
+        }
+
+        
+    }
+}
---
+++ Login/Source/Controllers/Receipts/Item.cs
@@ -0,0 +1,54 @@
+﻿namespace Login
+{
+    public enum Category
+    {
+        UNKNOWN_CATEGORY,
+        DAIRY_PRODUCTS,
+        BREAD_PRODUCTS,
+        DRINKS,
+        HOUSEHOLD_GOODS,
+        PET_PRODUCTS,
+        MEAT_AND_FISH,
+        FRUIT_AND_VEGETABLES,
+        SWEETS,
+        HOUSEHOLD_CHEMICALS,
+        HYGIENE_PRODUCTS,
+        OTHER_GOODS
+    };
+
+
+    public class Item
+    {
+        public int price { get; set; }
+        public string name { get; set; }
+        public Category category { get; set; }
+
+        // Required for XML serialization
+        public Item()
+        {
+
+        }
+
+        public Item(string name, int price)
+        {
+            this.price = price;
+            this.name = name.ToLower();
+        }
+
+        public double getPrice()
+        {
+            return price / (double)100;
+        }
+
+        public string GetName()
+        {
+            return this.name;
+
+        }
+
+        ~Item()
+        {
+
+        }
+    }
+}
