d8677d8de3237cc36a1c83fb3a49b1468a795dcb
Kuznecov Andrej <andrej.kuznecov@mif.stud.vu.lt>
2017-11-26 17:52:49 +0200

    first test commit

--- Login/Source/UI/MainActivity.cs
+++ Login/Source/UI/MainActivity.cs
@@ -37,11 +37,7 @@ namespace Login
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-
-
-
-
-
+            
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
             Button signUpBtn = FindViewById<Button>(Resource.Id.login_signUpButton);
